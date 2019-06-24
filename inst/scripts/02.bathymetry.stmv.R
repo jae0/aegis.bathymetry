1c2d5239c2d74aec3ba2c1cc99c1cd33a8b5c93b
Jae S Choi <choi.jae.seok@gmail.com>
2019-06-24 11:18:27 -0300

    update to new methods

--- inst/scripts/02.bathymetry.stmv.R
+++ inst/scripts/02.bathymetry.stmv.R
@@ -35,23 +35,26 @@ p = aegis.bathymetry::bathymetry_parameters(
   stmv_global_modelformula = "none",  # only marginally useful .. consider removing it and use "none",
   stmv_global_family ="none",
   stmv_local_modelengine="fft",
-  stmv_fft_filter = "lowpass_matern_tapered", #  act as a low pass filter first before matern .. depth has enough data for this. Otherwise, use:
+  stmv_fft_filter = "lowpass_matern_tapered", #  act as a low pass filter first before matern with taper .. depth has enough data for this. Otherwise, use:
   stmv_lowpass_nu = 0.5,
   stmv_lowpass_phi = 0.1,  # note: p$pres = 0.2
-  stmv_range_correlation_fft_taper = 0.01,  # in local smoothing convolutions occur of this correlation scale
+  stmv_fft_taper_factor = 5,  # in local smoothing convolutions taper to this areal expansion factor 1 ~ distance of one datum -- 5-6 seems optimal
   stmv_variogram_method = "fft",
-  stmv_variogram_nbreaks = 32,
+  stmv_variogram_nbreaks = 50,
+  stmv_discretized_n = 100,
+  stmv_range_correlation=0.1,
+  stmv_range_correlation_boostdata = c(0.01, 0.001, 0.0001),
   depth.filter = FALSE,  # need data above sea level to get coastline
   stmv_Y_transform =list(
     transf = function(x) {log10(x + 2500)} ,
-    invers = function(x) {10^(x - 2500)}
+    invers = function(x) {10^(x) - 2500}
   ), # data range is from -1667 to 5467 m: make all positive valued
-  stmv_rsquared_threshold = 0.75, # lower threshold
+  stmv_rsquared_threshold = 0.01, # lower threshold  .. ignore
   stmv_distance_statsgrid = 5, # resolution (km) of data aggregation (i.e. generation of the ** statistics ** )
-  stmv_distance_scale = c(5, 10, 15, 20), # km ... approx guess of 95% AC range
-  stmv_distance_prediction_fraction = 4/5, # i.e. 4/5 * 5 = 4 km
-  stmv_nmin = 500,  # min number of data points req before attempting to model in a localized space
-  stmv_nmax = 750, # no real upper bound.. just speed
+  stmv_distance_scale = c(5, 10, 20, 30, 40, 50, 60), # km ... approx guesses of 95% AC range
+  stmv_distance_prediction_fraction = 4/5, # i.e. 4/5 * 5 = 4 km .. relative to stats grid
+  stmv_nmin = 200,  # min number of data points req before attempting to model in a localized space
+  stmv_nmax = 800, # no real upper bound.. just speed /RAM
   stmv_clusters = list( scale=rep("localhost", scale_ncpus), interpolate=rep("localhost", interpolate_ncpus) )  # ncpus for each runmode
 )
 
