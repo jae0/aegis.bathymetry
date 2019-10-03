e907956de98a87df6ef2dc8ddfedf2924814f35c
Jae S Choi <choi.jae.seok@gmail.com>
2019-10-03 09:09:30 -0300

    add default params

--- inst/scripts/02.bathymetry.carstm.R
+++ inst/scripts/02.bathymetry.carstm.R
@@ -16,7 +16,7 @@ for ( areal_units_resolution_km in c(10, 20, 25) ) {
       areal_units_proj4string_planar_km = projection_proj4string("utm20")  # coord system to use for areal estimation and gridding for carstm
     )
     # sppoly = areal_units( p=p, redo=TRUE )  # this has already been done in aegis.polygons::01 polygons.R .. should nto have to redo
-    M = bathymetry.db( p=p, DS="aggregated_data", redo=TRUE )  # will redo if not found .. not used here but used for data matching/lookup in other aegis projects that use bathymetry
+    # M = bathymetry.db( p=p, DS="aggregated_data", redo=TRUE )  # already done in 01.bathymetry_data.R will redo if not found .. not used here but used for data matching/lookup in other aegis projects that use bathymetry
     M = bathymetry_carstm( p=p, DS="carstm_inputs", redo=TRUE )  # will redo if not found
   }
 }
