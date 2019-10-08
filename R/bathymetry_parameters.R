12358540a38dbe8fa1d07b8eb24bb489ea28ea20
Jae S Choi <choi.jae.seok@gmail.com>
2019-10-07 22:28:51 -0300

    add simplified parameter call

--- R/bathymetry_parameters.R
+++ R/bathymetry_parameters.R
@@ -9,7 +9,25 @@ bathymetry_parameters = function( p=NULL, project_name=NULL, project_class="defa
   if ( length(i) > 0 ) p = p[-i] # give any passed parameters a higher priority, overwriting pre-existing variable
 
 
+  if (project_class =="carstm_auid") {
+    # translate param values from one project to a unified representation
+    # must be first to catch p
+    P = bathymetry_parameters(
+      project_class = "carstm", # defines which parameter class / set to load
+      project_name = "bathymetry",
+      spatial_domain = p$spatial_domain,  # defines spatial area, currenty: "snowcrab" or "SSE"
+      areal_units_overlay = p$areal_units_overlay, # currently: "snowcrab_managementareas",  "groundfish_strata" .. additional polygon layers for subsequent analysis for now ..
+      areal_units_resolution_km = p$areal_units_resolution_km, # km dim of lattice ~ 1 hr
+      areal_units_proj4string_planar_km = p$areal_units_proj4string_planar_km,  # coord system to use for areal estimation and gridding for carstm
+      inputdata_spatial_discretization_planar_km = p$inputdata_spatial_discretization_planar_km,  # 1 km .. some thinning .. requires 32 GB RAM and limit of speed -- controls resolution of data prior to modelling to reduce data set and speed up modelling
+      auid = p$auid
+    )
+    return(P)
+  }
+
+
   # ---------------------
+
   # create/update library list
   p$libs = c( p$libs, RLibrary ( "colorspace",  "fields", "geosphere", "lubridate",  "lattice",
     "maps", "mapdata", "maptools", "parallel",  "rgdal", "rgeos",  "sp", "spdep", "splancs", "GADMTools" ) )
@@ -158,8 +176,8 @@ bathymetry_parameters = function( p=NULL, project_name=NULL, project_class="defa
             control.fixed=H$fixed,  # priors for fixed effects, generic is ok
             # control.inla=list(int.strategy="eb") ,# to get empirical Bayes results much faster.
             # control.inla=list( strategy="laplace", cutoff=1e-6, correct=TRUE, correct.verbose=FALSE ),
-            num.threads=4,
-            # blas.num.threads=1,
+            # num.threads=4,
+            blas.num.threads=8,
             verbose=TRUE
           ) ' )
       }
--- inst/scripts/02.bathymetry.carstm.R
+++ inst/scripts/02.bathymetry.carstm.R
@@ -4,11 +4,12 @@
 # construct basic parameter list defining the main characteristics of the study
 # and some plotting parameters (bounding box, projection, bathymetry layout, coastline)
   p = aegis.bathymetry::bathymetry_parameters(
+    project_name = "bathymetry",
     project_class = "carstm", # defines which parameter class / set to load
     spatial_domain = "SSE",  # defines spatial area, currenty: "snowcrab" or "SSE"
     inputdata_spatial_discretization_planar_km = 1,  # 1 km .. some thinning .. requires 32 GB RAM and limit of speed -- controls resolution of data prior to modelling to reduce data set and speed up modelling
     areal_units_resolution_km = 25, # km dim of lattice ~ 1 hr
-    areal_units_proj4string_planar_km = projection_proj4string("utm20"),  # coord system to use for areal estimation and gridding for carstm
+    areal_units_proj4string_planar_km = projection_proj4string("utm20")  # coord system to use for areal estimation and gridding for carstm
   )
 
 # example sequence to force creating of input data for modelling
@@ -26,8 +27,8 @@
   plot(fit)
   plot(fit, plot.prior=TRUE, plot.hyperparameters=TRUE, plot.fixed.effects=FALSE )
   s = summary(fit)
-  s$dic$dic
-  s$dic$p.eff
+  s$dic$dic # 1404489
+  s$dic$p.eff # 151412
 
 # maps of some of the results
   carstm_plot( p=p, res=res, vn="z.predicted" )
