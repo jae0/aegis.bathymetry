0ff770df7367820f7e749f38e74b73df94c5e0df
Jae S Choi <choi.jae.seok@gmail.com>
2019-10-18 10:11:02 -0300

    params tweak

--- inst/scripts/02.bathymetry.stmv.R
+++ inst/scripts/02.bathymetry.stmv.R
@@ -28,7 +28,7 @@ p = aegis.bathymetry::bathymetry_parameters(
   data_root = project.datadirectory( "aegis", "bathymetry" ),
   DATA = 'bathymetry.db( p=p, DS="stmv_inputs" )',
   variables = list(Y="z"),  # required as fft has no formulae
-  inputdata_spatial_discretization_planar_km = 0.05,  # 0.2==p$pres; controls resolution of data prior to modelling (km .. ie 20 linear units smaller than the final discretization pres)
+  inputdata_spatial_discretization_planar_km = 0.2,  # 0.2==p$pres; controls resolution of data prior to modelling (km .. ie 20 linear units smaller than the final discretization pres)
   spatial_domain = "canada.east.superhighres",
   spatial_domain_subareas = c( "canada.east.highres", "canada.east",  "SSE", "SSE.mpa" , "snowcrab"),
   aegis_dimensionality="space",
