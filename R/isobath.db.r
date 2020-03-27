9df1f21f065067f016d486dc0597eba1fe9b5072
Jae S Choi <choi.jae.seok@gmail.com>
2020-03-27 15:49:41 -0300

    save location logic

--- R/isobath.db.r
+++ R/isobath.db.r
@@ -3,10 +3,12 @@ isobath.db = function( ip=NULL, p=NULL, depths=c(100, 200), DS="isobath", crs=pr
   #\\ create or return isobaths and coastlines/coast polygons
   # require(stmv)
   if (DS %in% c( "isobath", "isobath.redo" )) {
+
     fn = paste("isobaths", p$spatial_domain, "rdata", sep=".")
-    if ( is.null(datadir) )  datadir = project.datadirectory( "aegis", "bathymetry" )
+
+    if ( is.null(datadir) ) datadir = project.datadirectory( "aegis", "bathymetry" )
+
     fn.iso = file.path( datadir, "isobaths", fn )  # in case there is an alternate project
-    if (!file.exists( fn.iso )) fn.iso = file.path( datadir, "isobaths", fn )  # in case there is an alternate project
 
     isobaths = NULL
     notfound = NULL
