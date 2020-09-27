029e6e083ea68bcc93d3642ed2efc0b50868c547
Jae S Choi <choi.jae.seok@gmail.com>
2020-09-27 13:37:40 -0300

    maup

--- R/bathymetry_carstm.R
+++ R/bathymetry_carstm.R
@@ -17,7 +17,7 @@
         variabletomodel ="z",
         carstm_model_label = "production",
         inputdata_spatial_discretization_planar_km = 1,  # 1 km .. some thinning .. requires 32 GB RAM and limit of speed -- controls resolution of data prior to modelling to reduce data set and speed up modelling
-        areal_units_resolution_km = 2, # 25 km dim of lattice ~ 1 hr; 5km = 79hrs
+        areal_units_resolution_km = 2, # 25 km dim of lattice ~ 1 hr; 5km = 79hrs; 2km = ?? hrs
         areal_units_proj4string_planar_km = aegis::projection_proj4string("utm20"),  # coord system to use for areal estimation and gridding for carstm
         areal_units_source = "lattice", # "stmv_fields" to use ageis fields instead of carstm fields ... note variables are not the same
         areal_units_overlay = "none"
--- inst/scripts/02.bathymetry.carstm.R
+++ inst/scripts/02.bathymetry.carstm.R
@@ -10,6 +10,26 @@
   p = aegis.bathymetry::bathymetry_carstm( DS = "parameters_production" )
     # DS = "parameters_production"; areal_units_resolution_km=5 ... takes 79 Hrs!
 
+  maup = map_area_unit_problem( just_return_results=TRUE )  #default is bathymetry data
+
+  if (0) {
+
+    x = maup$resolution
+    # x = log(  maup$n / maup$resolution^2  )  # data density
+    yrange = range( maup$min, maup$max )
+    plot(mean ~ x, maup, pch=20, ylim=yrange)
+    lines( median ~ x, maup, col="green", lwd=1)
+    lines(min ~ x, maup, col="red", lwd=4)
+    lines(max ~ x, maup, col="blue", lwd=4)
+    lines( I(mean+sd) ~ x, maup, col="gray", lwd=2)
+    lines( I(mean-sd) ~ x, maup, col="gray", lwd=2)
+    abline( v=attr(maup, "variogram")$fft$localrange ) # 380 km
+
+    plot( sd~ x, maup)
+    abline( v=attr(maup, "variogram")$fft$localrange ) # 380 km
+
+  }
+
 # example sequence to force creating of input data for modelling
   sppoly = areal_units( p=p, redo=TRUE ); plot(sppoly) # or: spplot( sppoly, "AUID", main="AUID", sp.layout=p$coastLayout )
   M = bathymetry_db( p=p, DS="aggregated_data" , redo=TRUE )  # will redo if not found .. not used here but used for data matching/lookup in other aegis projects that use bathymetry
