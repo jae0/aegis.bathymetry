e3388bf82b3240f9c08841388913b7862108592b
Jae S Choi <choi.jae.seok@gmail.com>
2021-06-20 14:53:48 -0300

    alt default

--- R/bathymetry_parameters.R
+++ R/bathymetry_parameters.R
@@ -34,7 +34,7 @@ bathymetry_parameters = function( p=list(), project_name="bathymetry", project_c
 
   p = spatial_parameters( p=p )  # default (= only supported resolution of 0.2 km discretization)  .. do NOT change
 
-  p = parameters_add_without_overwriting( p, inputdata_spatial_discretization_planar_km = 1 ) #  p$pres/2 is a bit too slow ..; controls resolution of data prior to modelling (km .. ie 20 linear units smaller than the final discretization pres)
+  p = parameters_add_without_overwriting( p, inputdata_spatial_discretization_planar_km = 0.5 ) #  p$pres/2 is a bit too slow ..; controls resolution of data prior to modelling (km .. ie 20 linear units smaller than the final discretization pres)
 
 
   # ---------------------
